9ba2f80e6f77510165e0db075042d0162b64b34e
Mcarmen <maricarmenarenas98@hotmail.es>
2020-12-14 21:26:49 +0100

    fix(): connect and disconnect

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ exports.app = app;
 
 // socket
 const io = require('socket.io')(process.env.SOCKET_PORT, {transports: ['websocket']});
-const chatNamespace = io.of('/chat');
 const jwt = require('jsonwebtoken');
 const redis = require("redis");
 
@@ -42,44 +41,37 @@ const clientRedis = redis.createClient(6379, process.env.REDIS_HOST);
 
 // Authentication middleware
 io.of('/chat').use((socket, next) => {
-  console.log("Aaaaaaaaaaaaaaaaaa")
   if (!(socket.handshake.query && socket.handshake.query.token)) {
     next(new Error('Authentication error'));
   }
 
   let token = socket.handshake.query.token
-  console.log('TOKEN: ', token)
   jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
     if (err) return next(new Error('Authentication error'));
     socket.decoded = decoded;
     next();
   });    
 })
+
 // Connection authenticated 
-chatNamespace.on('connection', function(socket) {
-  //socket lo almacenas -> socket.decode.userId
-  clientRedis.sadd(`user:${socket.decode.userId}`, {
-     socketId: socket.id
-  });
+ io.of('/chat').on('connection', function(socket) {
+  clientRedis.sadd(`user:${socket.decoded.userId}`, JSON.stringify({ socketId: socket.id }));
 
   socket.on('message', function(data) {
     // emit message to the user (id)
-    chatNamespace.to(socketId).emit('message', data.content);
+    let socketIds = clientRedis.smembers(`user:${data.userId}`);
+    socketIds.map(x => {
+       io.of('/chat').to(x.socketId).emit('message', data.content);
+    });
+
     // create messages in redis
 
     // modify the last message 
 
   });
-});
 
-chatNamespace.on('disconnect', function(socket) {
-  //socket lo eliminas -> socket.decode.userId
-  socket.id
+  socket.on('disconnect', () => {
+   clientRedis.srem(`user:${socket.decoded.userId}`, JSON.stringify({ socketId: socket.id }));
+  });
 });
-
-app.get('/', (req, res) => {
-  const chatNamespace = io.of('/chat');
-  chatNamespace.emit('anevent', { some: 'data' });
-  res.send('tttes')
-})
 
